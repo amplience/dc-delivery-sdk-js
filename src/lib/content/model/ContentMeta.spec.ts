e50c8be908f78363017a7594c914c27e482fdb76
Marc Easen <measen@amplience.com>
2020-03-03 13:48:07 +0000

    feat(content-meta): exposes the delivery key property to support content items publushed using content-delivery-v2

--- src/lib/content/model/ContentMeta.spec.ts
+++ src/lib/content/model/ContentMeta.spec.ts
@@ -8,6 +8,8 @@ describe('ContentMeta', () => {
         schema:
           'https://raw.githubusercontent.com/techiedarren/dc-examples/master/content-types/containers/page.json',
         name: 'Homepage Body',
+        deliveryId: '00112233-4455-6677-8899-aabbccddeeff',
+        deliveryKey: 'homepage/body',
         edition: {
           id: '5b1a621ac9e77c0001b121b4',
           start: '2018-06-30T23:00:00.000Z',
@@ -21,6 +23,13 @@ describe('ContentMeta', () => {
       expect(meta.schema).to.eq(
         'https://raw.githubusercontent.com/techiedarren/dc-examples/master/content-types/containers/page.json'
       );
+      expect(meta.name).to.eq('Homepage Body');
+      expect(meta.deliveryId).to.eq('00112233-4455-6677-8899-aabbccddeeff');
+      expect(meta.deliveryKey).to.eq('homepage/body');
+      expect(meta.edition.id).to.eq('5b1a621ac9e77c0001b121b4');
+      expect(meta.edition.start).to.eq('2018-06-30T23:00:00.000Z');
+      expect(meta.edition.end).to.eq('2018-07-06T22:59:59.999Z');
+      expect(meta.lifecycle.expiryTime).to.eq('2018-07-06T22:59:59.999Z');
     });
 
     it('should serialize to json', () => {
@@ -28,6 +37,8 @@ describe('ContentMeta', () => {
         schema:
           'https://raw.githubusercontent.com/techiedarren/dc-examples/master/content-types/containers/page.json',
         name: 'Homepage Body',
+        deliveryId: '00112233-4455-6677-8899-aabbccddeeff',
+        deliveryKey: 'homepage/body',
         edition: {
           id: '5b1a621ac9e77c0001b121b4',
           start: '2018-06-30T23:00:00.000Z',
@@ -42,6 +53,8 @@ describe('ContentMeta', () => {
         schema:
           'https://raw.githubusercontent.com/techiedarren/dc-examples/master/content-types/containers/page.json',
         name: 'Homepage Body',
+        deliveryId: '00112233-4455-6677-8899-aabbccddeeff',
+        deliveryKey: 'homepage/body',
         edition: {
           id: '5b1a621ac9e77c0001b121b4',
           start: '2018-06-30T23:00:00.000Z',
