3669f5d18c1893cfe6f53730e0b75080c6799405
Marc Easen <measen@amplience.com>
2020-06-16 09:24:30 +0100

    test(ContentClient): added getContentItemById & getContentItemByKey tests

--- src/lib/ContentClient.spec.ts
+++ src/lib/ContentClient.spec.ts
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
 import { ContentClient } from '../index';
 import MockAdapter from 'axios-mock-adapter';
-import * as SINGLE_RESULT from './content/coordinators/__fixtures__/v1/SINGLE_RESULT.json';
+import * as V1_SINGLE_RESULT from './content/coordinators/__fixtures__/v1/SINGLE_RESULT.json';
+import * as V2_SINGLE_RESULT from './content/coordinators/__fixtures__/v2/SINGLE_RESULT.json';
 
 describe('ContentClient', () => {
   context('configuration', () => {
@@ -44,7 +45,7 @@ describe('ContentClient', () => {
           .onGet(
             'https://c1.adis.ws/cms/content/query?query=%7B%22sys.iri%22%3A%22http%3A%2F%2Fcontent.cms.amplience.com%2F2c7efa09-7e31-4503-8d00-5a150ff82f17%22%7D&fullBodyObject=true&scope=tree&store=test'
           )
-          .reply(200, SINGLE_RESULT);
+          .reply(200, V1_SINGLE_RESULT);
 
         const client = new ContentClient({
           account: 'test',
@@ -63,6 +64,112 @@ describe('ContentClient', () => {
               'https://raw.githubusercontent.com/techiedarren/dc-examples/master/content-types/containers/page.json'
           }
         });
+      });
+    });
+    context('ContentClientConfigV2', () => {
+      it('should resolve if content item is found', async () => {
+        const mocks = new MockAdapter(null);
+        mocks
+          .onGet(
+            'https://test.cdn.content.amplience.net/content/id/0bf85aa1-9386-4068-adad-6b9a813f5ddb?depth=all&format=inlined'
+          )
+          .reply(200, V2_SINGLE_RESULT);
+
+        const client = new ContentClient({
+          hubName: 'test',
+          adaptor: mocks.adapter()
+        });
+
+        const response = await client.getContentItem(
+          '0bf85aa1-9386-4068-adad-6b9a813f5ddb'
+        );
+
+        expect(response.toJSON()).to.deep.eq(V2_SINGLE_RESULT['content']);
+      });
+    });
+  });
+
+  context('getContentItemById', () => {
+    context('ContentClientConfigV1', () => {
+      it('should resolve if content item is found', async () => {
+        const mocks = new MockAdapter(null);
+        mocks
+          .onGet(
+            'https://c1.adis.ws/cms/content/query?query=%7B%22sys.iri%22%3A%22http%3A%2F%2Fcontent.cms.amplience.com%2F2c7efa09-7e31-4503-8d00-5a150ff82f17%22%7D&fullBodyObject=true&scope=tree&store=test'
+          )
+          .reply(200, V1_SINGLE_RESULT);
+
+        const client = new ContentClient({
+          account: 'test',
+          adaptor: mocks.adapter()
+        });
+
+        const response = await client.getContentItemById(
+          '2c7efa09-7e31-4503-8d00-5a150ff82f17'
+        );
+
+        expect(response.toJSON()).to.deep.eq({
+          _meta: {
+            deliveryId: '2c7efa09-7e31-4503-8d00-5a150ff82f17',
+            name: 'name',
+            schema:
+              'https://raw.githubusercontent.com/techiedarren/dc-examples/master/content-types/containers/page.json'
+          }
+        });
+      });
+    });
+    context('ContentClientConfigV2', () => {
+      it('should resolve if content item is found', async () => {
+        const mocks = new MockAdapter(null);
+        mocks
+          .onGet(
+            'https://test.cdn.content.amplience.net/content/id/0bf85aa1-9386-4068-adad-6b9a813f5ddb?depth=all&format=inlined'
+          )
+          .reply(200, V2_SINGLE_RESULT);
+
+        const client = new ContentClient({
+          hubName: 'test',
+          adaptor: mocks.adapter()
+        });
+
+        const response = await client.getContentItemById(
+          '0bf85aa1-9386-4068-adad-6b9a813f5ddb'
+        );
+
+        expect(response.toJSON()).to.deep.eq(V2_SINGLE_RESULT['content']);
+      });
+    });
+  });
+
+  context('getContentItemByKey', () => {
+    context('ContentClientConfigV1', () => {
+      it('should resolve if content item is found', async () => {
+        const client = new ContentClient({
+          account: 'test'
+        });
+
+        expect(() => client.getContentItemByKey('content-key')).to.throw(
+          'Not supported. You need to define "hubName" configuration property to use getContentItemByKey()'
+        );
+      });
+    });
+    context('ContentClientConfigV2', () => {
+      it('should resolve if content item is found', async () => {
+        const mocks = new MockAdapter(null);
+        mocks
+          .onGet(
+            'https://test.cdn.content.amplience.net/content/key/welcome-para-1?depth=all&format=inlined'
+          )
+          .reply(200, V2_SINGLE_RESULT);
+
+        const client = new ContentClient({
+          hubName: 'test',
+          adaptor: mocks.adapter()
+        });
+
+        const response = await client.getContentItemByKey('welcome-para-1');
+
+        expect(response.toJSON()).to.deep.eq(V2_SINGLE_RESULT['content']);
       });
     });
   });
